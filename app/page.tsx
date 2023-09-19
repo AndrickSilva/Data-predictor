9a50b2a0a4fe7e5cae40a9f2caabbf2979b28e60
AJ <silva.andrick2001@gamil.com>
2023-09-19 13:00:27 +0530

    added details styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,25 @@ import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("")
+  const [Loading, setLoading] = useState(false)
   const {push} = useRouter();
   
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    setLoading(true)
     push(`/prediction/${inputValue}`)
     setInputValue("")
   }
   return (
     <main className="flex justify-center items-center flex-col min-h-screen bg-slate-700">
-      <div className="flex flex-col bg-slate-800 p-8 rounded-md">
+      <div className="flex flex-col bg-slate-800 p-8 rounded-md shadow-lg">
         <h1 className='text-orange-500 mb-4 font-bold text-xl'>Enter Your name</h1>
-        <form className='flex flex-col items-start' onSubmit={handleSubmit}>
+        <form className='flex flex-col ' onSubmit={handleSubmit}>
           <input className='h-10 rounded-md px-4 mb-4' type="text" value={inputValue} placeholder='Enter your name..' onChange={(e) => setInputValue(e.target.value)} />
           <button className='bg-orange-500 text-slate-200 p-2 rounded-md' type='submit'>Submit</button>
         </form>
       </div>
+        {Loading && <p className='text-slate-200'>Please wait..</p>}
     </main>
   )
 }
--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -27,11 +27,17 @@ const page = async ({ params }: Params) => {
     ])
 
     return (
-        <div>
-            <p>params : {params.name}</p>
-            <p>age : {age?.age}</p>
-            <p>gender : {gender?.gender}</p>
-            <p>country : {country?.country[0]?.country_id}</p>
+        <div className="flex justify-center items-center flex-col min-h-screen bg-slate-700">
+            <div className="flex flex-col bg-slate-800 py-6 px-12 rounded-lg shadow-md">
+                <h2 className="text-orange-500 text-2xl font-bold mb-4">Personal Info</h2>
+                <div className="text-slate-200">
+                    <p>Params : {params.name}</p>
+                    <p>Age : {age?.age}</p>
+                    <p>Gender : {gender?.gender}</p>
+                    <p>Country : {country?.country[0]?.country_id}</p>
+                </div>
+            </div>
+                <button className="text-slate-200 mt-4 underline underline-offset-4"><a href="/">Try Again</a></button>
         </div>
     )
 }
